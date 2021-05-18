5fe2e29e009c6eed93d49fbbc2a9e74b0d1afe20
Jordyn Young <29105443+JordynY@users.noreply.github.com>
2021-05-18 23:50:47 +0100

    Initial commit

---
+++ app/src/main/java/com/midnightwick/forged/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.midnightwick.forged
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+class Skill(
+
+)
