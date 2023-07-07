63131445070b4df84ec0d44a06b6de11aa285a18
Jade Neoma <Joung3010@gmail.com>
2023-07-07 05:04:40 +0100

    Added License

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
+/*
+ *     Copyright (c) 2023 Jade Neoma <Joung3010@gmail.com>
+ *
+ *
+ *     This file is part of Forged.
+ *
+ *     Forged is free software: you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public
+ *     License as published by the Free Software Foundation,
+ *     either version 3 of the License, or (at your option) any
+ *     later version.
+ *
+ *     Forged is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the
+ *     implied warranty of MERCHANTABILITY or FITNESS
+ *     FOR A PARTICULAR PURPOSE. See the GNU General
+ *     Public License for more details.
+ *
+ *     You should have received a copy of the GNU General
+ *     Public License along with Forged. If not, see
+ *     <https://www.gnu.org/licenses/>.
+ */
+
+
 fun main() {
 
 }
