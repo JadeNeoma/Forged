87f1311e09709c9b24af0ba99b1f4e8453f16522
Jade Neoma <Joung3010@gmail.com>
2023-07-07 06:19:11 +0100

    Change To Gradle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,28 @@
-/*
- *     Copyright (c) 2023 Jade Neoma <Joung3010@gmail.com>
- *
- *
- *     This file is part of Forged.
- *
- *     Forged is free software: you can redistribute it and/or
- *     modify it under the terms of the GNU General Public
- *     License as published by the Free Software Foundation,
- *     either version 3 of the License, or (at your option) any
- *     later version.
- *
- *     Forged is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the
- *     implied warranty of MERCHANTABILITY or FITNESS
- *     FOR A PARTICULAR PURPOSE. See the GNU General
- *     Public License for more details.
- *
- *     You should have received a copy of the GNU General
- *     Public License along with Forged. If not, see
- *     <https://www.gnu.org/licenses/>.
- */
+import java.awt.EventQueue
+import javax.swing.JFrame
 
+class ForgedApp(title: String) : JFrame() {
+
+    init {
+        createUI(title)
+    }
+
+    private fun createUI(title: String) {
+
+        setTitle(title)
+
+        defaultCloseOperation = EXIT_ON_CLOSE
+        setSize(400, 300)
+        setLocationRelativeTo(null)
+    }
+}
+
+private fun createAndShowGUI() {
+
+    val frame = ForgedApp("Forged")
+    frame.isVisible = true
+}
 
 fun main() {
-
+    EventQueue.invokeLater(::createAndShowGUI)
 }
