da547bbf826b1b21c3dafeceda532bd83c8ec855
Jade Neoma <Joung3010@gmail.com>
2023-07-08 20:38:28 +0100

    I have no idea how to do this

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,8 +1,8 @@
 /*
- *     Copyright (c) 2023 Jade Neoma <Joung3010@gmail.com>
+ *      Copyright (c) 2023 Jade Neoma <Joung3010@gmail.com>
  *
  *
- *     This file is part of Forged.
+ *      This file is part of Forged.
  *
  *      Forged is free software: you can redistribute it and/or
  *      modify it under the terms of the GNU General Public
@@ -23,132 +23,214 @@
 
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.*
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.MenuBar
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberDialogState
 
 @Composable
 @Preview
-fun App(system : TTRPG) {
+fun App(ttrpg: TTRPG) {
     MaterialTheme {
 
-    }
-}
-
-@Composable
-@Preview
-fun NewSystemDialog(){
-    MaterialTheme {
-        var systemName by remember { mutableStateOf("Example System") }
-        var authorName by remember { mutableStateOf("Example Author") }
+        Text("Hello World")
+        var websDropDown by remember { mutableStateOf(false) }
+        var weblength by remember { mutableStateOf(ttrpg.webs.size) }
+        var webSelected by remember { mutableStateOf(0) }
         Column(
-            modifier = Modifier
-                .fillMaxHeight()
-                .padding(50.dp),
-            verticalArrangement = Arrangement.SpaceEvenly
-        ){
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = systemName,
-                onValueChange = { systemName = it },
-                label = { Text(text = "System Name") },
+            modifier = Modifier.fillMaxSize(0.5f)
+        ) {
+            if (weblength == 0) {
+                Text(
+                    text = "Go to 'Edit' then 'Create Web' to get started",
+                    modifier = Modifier.fillMaxWidth().background(Color.Gray)
                 )
-
-            TextField(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                value = authorName,
-                onValueChange = { authorName = it },
-                label = { Text(text = "Author") },
-            )
-
-            Button(
-                content = {Text("Create System!")},
-                onClick = {createNew(systemName, authorName)},
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-            )
+            } else {
+                Text(
+                    text = remember { ttrpg.webs[webSelected].name },
+                    modifier = Modifier.fillMaxWidth().clickable(onClick = { websDropDown = true })
+                        .background(Color.Gray)
+                )
+            }
+            DropdownMenu(
+                onDismissRequest = { websDropDown = false },
+                expanded = websDropDown
+            ) {
+                ttrpg.webs.forEachIndexed { i, each ->
+                    DropdownMenuItem(
+                        onClick = { webSelected = i },
+                        content = { Text(each.name) }
+                    )
+                }
+                DropdownMenuItem(
+                    onClick = {},
+                    content = { Text("Hello World") }
+                )
+            }
         }
     }
 }
 
-@Composable
-fun createNew(systemName: String, authorName: String){
-    App(TTRPG(systemName, authorName))
-}
 
 fun main() = application {
+    var winTitle by remember { mutableStateOf("") }
     Window(
-
+        title = winTitle,
         onCloseRequest = ::exitApplication
     ) {
-
         var isNewDialogOpen by remember { mutableStateOf(false) }
         var isCopyrightDialogOpen by remember { mutableStateOf(false) }
+        var notImplemented by remember { mutableStateOf(false) }
+        var isLoaded by remember { mutableStateOf(false) }
+        var systemName by remember { mutableStateOf("Example System") }
+        var authorName by remember { mutableStateOf("Example Author") }
+        var ttrpg = TTRPG("empty", "empty")
+        var reload by remember { mutableStateOf(false) }
 
         MenuBar {
-            Menu("File"){
+            Menu("File") {
                 Item("New", onClick = { isNewDialogOpen = true })
-                Item("Load", onClick = {}) // TODO: Add loading functionality
-                Item("Exit", onClick = {exitApplication()})
+                Item("Load", onClick = { notImplemented = true }) // TODO: Add loading functionality
+                if (isLoaded) {
+                    Item("Save", onClick = { notImplemented = true })
+                }
+                Item("Exit", onClick = { exitApplication() })
             }
 
-            Menu("Help"){
-                Item("Manual", onClick = {})
-                Item("Copyright", onClick = {isCopyrightDialogOpen = true}) // TODO: Add loading functionality
+            if (isLoaded) {
+                Menu("Edit") {
+                    Item("Create Web", onClick = {
+                        ttrpg.addWeb("test", "first", "second", "third")
+                        reload = true
+                    })
+                }
+            }
+
+            Menu("Help") {
+                Item("Manual", onClick = { notImplemented = true }) // TODO: Add manual link
+                Item("Copyright", onClick = { isCopyrightDialogOpen = true })
             }
 
             if (isCopyrightDialogOpen) {
                 Dialog(
                     title = "Copyright",
-                    onCloseRequest = {isCopyrightDialogOpen = false},
+                    onCloseRequest = { isCopyrightDialogOpen = false },
                     state = rememberDialogState(position = WindowPosition(Alignment.Center))
-                ){
-                    Text("Copyright (c) 2023 Jade Neoma\n" +
-                            "Forged is free software: you can redistribute it and/or\n" +
-                            "modify it under the terms of the GNU General Public\n" +
-                            "License as published by the Free Software Foundation,\n" +
-                            "either version 3 of the License, or (at your option) any\n" +
-                            "later version.\n" +
-                            "\n" +
-                            "Forged is distributed in the hope that it will be useful,\n" +
-                            "but WITHOUT ANY WARRANTY; without even the\n" +
-                            "implied warranty of MERCHANTABILITY or FITNESS\n" +
-                            "FOR A PARTICULAR PURPOSE. See the GNU General\n" +
-                            "Public License for more details.\n" +
-                            "\n" +
-                            "You should have received a copy of the GNU General\n" +
-                            "Public License along with Forged. If not, see\n" +
-                            "<https://www.gnu.org/licenses/>."
+                ) {
+                    Text(
+                        "Copyright (c) 2023 Jade Neoma\n" +
+                                "Forged is free software: you can redistribute it and/or\n" +
+                                "modify it under the terms of the GNU General Public\n" +
+                                "License as published by the Free Software Foundation,\n" +
+                                "either version 3 of the License, or (at your option) any\n" +
+                                "later version.\n" +
+                                "\n" +
+                                "Forged is distributed in the hope that it will be useful,\n" +
+                                "but WITHOUT ANY WARRANTY; without even the\n" +
+                                "implied warranty of MERCHANTABILITY or FITNESS\n" +
+                                "FOR A PARTICULAR PURPOSE. See the GNU General\n" +
+                                "Public License for more details.\n" +
+                                "\n" +
+                                "You should have received a copy of the GNU General\n" +
+                                "Public License along with Forged. If not, see\n" +
+                                "<https://www.gnu.org/licenses/>."
                     )
+                }
+            }
+
+            if (notImplemented) {
+                Dialog(
+                    onCloseRequest = { notImplemented = false }
+                ) {
+                    Text("Not Implemented")
                 }
             }
 
             if (isNewDialogOpen) {
                 Dialog(
                     title = "New System",
-                    onCloseRequest = {isNewDialogOpen = false},
+                    onCloseRequest = { isNewDialogOpen = false },
                     state = rememberDialogState(position = WindowPosition(Alignment.Center))
-                ){
-                    NewSystemDialog()
+                ) {
+                    var load by remember { mutableStateOf(false) }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(50.dp),
+                        verticalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        TextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = systemName,
+                            onValueChange = { systemName = it },
+                            label = { Text(text = "System Name") },
+                        )
+
+                        TextField(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            value = authorName,
+                            onValueChange = { authorName = it },
+                            label = { Text(text = "Author") },
+                        )
+
+                        Button(
+                            content = { Text("Create System!") },
+                            onClick = { load = true },
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                        )
+
+                        if (load) {
+                            isNewDialogOpen = false
+                            isLoaded = true
+                            var ttrpg = TTRPG(systemName, authorName)
+                            winTitle = systemName
+                        }
+
+                    }
                 }
             }
         }
+        if (isLoaded) {
+            App(ttrpg)
+        }
     }
+
 }
 
 class Anchor(
-    var name:String,
+    var name: String,
     val x: Double,
     val y: Double,
-){
+) {
     override fun toString(): String {
         return "$name: ($x, $y)"
     }
@@ -159,12 +241,13 @@ class Web(
     anchor0: String,
     anchor1: String,
     anchor2: String
-){
-    var anchors: MutableList<Anchor> =
-        mutableListOf(
+) {
+    var anchors: SnapshotStateList<Anchor> =
+        mutableStateListOf(
             Anchor(anchor0, 0.0, 433.013),
             Anchor(anchor1, 500.0, -433.013),
-            Anchor(anchor2, -500.0, -433.013) )
+            Anchor(anchor2, -500.0, -433.013)
+        )
 
     public fun createAnchor() {
 
@@ -179,14 +262,20 @@ class Web(
 class TTRPG(
     var name: String,
     var author: String,
-    var webs: MutableList<Web> = mutableListOf()
-){
+    var webs: SnapshotStateList<Web> = mutableStateListOf()
+) {
     init {
         val created = System.currentTimeMillis()
     }
 
     override fun toString(): String {
         return "name: $name\n author: $author\n Webs:\n $webs "
+    }
+
+    fun addWeb(webName: String, anchor1: String, anchor2: String, anchor3: String) {
+        mutableStateListOf<Web>().apply {
+            add(Web(webName, anchor1, anchor2, anchor3))
+        }
     }
 
 }
