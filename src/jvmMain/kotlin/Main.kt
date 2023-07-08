f748f5ccd116dce5b4a29aa1ce32da30e95d2a86
Jade Neoma <Joung3010@gmail.com>
2023-07-08 04:10:07 +0100

    Setup commit

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,8 +1,8 @@
 /*
- *     Copyright (c) 2023 Jade Neoma <Joung3010@gmail.com>
+ *      Copyright (c) 2023 Jade Neoma <Joung3010@gmail.com>
  *
  *
- *     This file is part of Forged.
+ *      This file is part of Forged.
  *
  *      Forged is free software: you can redistribute it and/or
  *      modify it under the terms of the GNU General Public
@@ -35,17 +35,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.key.Key.Companion.Menu
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
-import javax.swing.GroupLayout
-import javax.swing.text.Position
-import kotlin.system.exitProcess
 
 @Composable
 @Preview
-fun App() {
+fun App(system : TTRPG) {
+    MaterialTheme {
 
+    }
 }
 
 @Composable
@@ -53,10 +51,12 @@ fun App() {
 fun NewSystemDialog(){
     MaterialTheme {
         var systemName by remember { mutableStateOf("Example System") }
+        var authorName by remember { mutableStateOf("Example Author") }
         Column(
             modifier = Modifier
                 .fillMaxHeight()
-                .padding(50.dp)
+                .padding(50.dp),
+            verticalArrangement = Arrangement.SpaceEvenly
         ){
             TextField(
                 modifier = Modifier.fillMaxWidth(),
@@ -65,19 +65,26 @@ fun NewSystemDialog(){
                 label = { Text(text = "System Name") },
                 )
 
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                value = authorName,
+                onValueChange = { authorName = it },
+                label = { Text(text = "Author") },
+            )
+
             Button(
                 content = {Text("Create System!")},
                 onClick = {createNew(systemName)},
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
-                    .padding(0.dp, 50.dp, 0.dp, 0.dp)
             )
         }
     }
 }
 
 fun createNew(systemName:String){
-    print(systemName)
+
 }
 
 fun main() = application {
@@ -87,7 +94,7 @@ fun main() = application {
     ) {
 
         var isNewDialogOpen by remember { mutableStateOf(false) }
-        var isLicenceDialogOpen by remember { mutableStateOf(false) }
+        var isCopyrightDialogOpen by remember { mutableStateOf(false) }
 
         MenuBar {
             Menu("File"){
@@ -98,16 +105,32 @@ fun main() = application {
 
             Menu("Help"){
                 Item("Manual", onClick = {})
-                Item("License", onClick = {isLicenceDialogOpen = true}) // TODO: Add loading functionality
+                Item("Copyright", onClick = {isCopyrightDialogOpen = true}) // TODO: Add loading functionality
             }
 
-            if (isLicenceDialogOpen) {
+            if (isCopyrightDialogOpen) {
                 Dialog(
-                    title = "License",
-                    onCloseRequest = {isLicenceDialogOpen = false},
+                    title = "Copyright",
+                    onCloseRequest = {isCopyrightDialogOpen = false},
                     state = rememberDialogState(position = WindowPosition(Alignment.Center))
                 ){
-
+                    Text("Copyright (c) 2023 Jade Neoma\n" +
+                            "Forged is free software: you can redistribute it and/or\n" +
+                            "modify it under the terms of the GNU General Public\n" +
+                            "License as published by the Free Software Foundation,\n" +
+                            "either version 3 of the License, or (at your option) any\n" +
+                            "later version.\n" +
+                            "\n" +
+                            "Forged is distributed in the hope that it will be useful,\n" +
+                            "but WITHOUT ANY WARRANTY; without even the\n" +
+                            "implied warranty of MERCHANTABILITY or FITNESS\n" +
+                            "FOR A PARTICULAR PURPOSE. See the GNU General\n" +
+                            "Public License for more details.\n" +
+                            "\n" +
+                            "You should have received a copy of the GNU General\n" +
+                            "Public License along with Forged. If not, see\n" +
+                            "<https://www.gnu.org/licenses/>."
+                    )
                 }
             }
 
@@ -121,6 +144,55 @@ fun main() = application {
                 }
             }
         }
-        App()
     }
+}
+
+class Anchor(
+    var name:String,
+    val x: Double,
+    val y: Double,
+){
+    override fun toString(): String {
+        return "$name: $x, $y"
+    }
+}
+
+class Web(
+    var name: String,
+    var anchors: MutableList<Anchor>,
+    anchor0: String,
+    anchor1: String,
+    anchor2: String
+){
+    init{
+        val default : Array<Array<Double>> = arrayOf(
+            arrayOf(0.0, 433.013),
+            arrayOf(500.0, -433.013),
+            arrayOf(-500.0, 433.013) )
+        anchors.add(Anchor(anchor0, default[0][0], default[0][1]))
+        anchors.add(Anchor(anchor1, default[1][0], default[1][1]))
+        anchors.add(Anchor(anchor2, default[2][0], default[2][1]))
+    }
+
+
+    override fun toString(): String {
+        return "name: $name\n anchors:\n $anchors"
+    }
+}
+
+class TTRPG(
+    var name: String,
+    var author: String,
+    var webs: MutableList<Web>
+){
+    init {
+        val created = System.currentTimeMillis()
+    }
+
+    override fun toString(): String {
+        return "name: $name\n author: $author\n Webs:\n $webs "
+    }
+
+
+
 }
